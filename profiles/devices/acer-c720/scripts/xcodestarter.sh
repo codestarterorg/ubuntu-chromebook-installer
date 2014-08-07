3f2e41319c215eb5ff298d2a455675253df1711d
Sam Gleske <sam.mxracer@gmail.com>
2014-08-06 20:34:40 -0400

    bracket vars in acer xcodestarter.sh

--- profiles/devices/acer-c720/scripts/xcodestarter.sh
+++ profiles/devices/acer-c720/scripts/xcodestarter.sh
@@ -8,49 +8,49 @@ tempbuild="$(mktemp -d)"
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing Chrome"
-cd "$tempbuild"
+cd "${tempbuild}"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 apt-get -f -y -q install
 touch "chrome.done"
 
 echo "Installing Scratch"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install scratch
 touch "scratch.done"
 
 echo "Installing Sublime Text 3"
-cd "$tempbuild"
+cd "${tempbuild}"
 wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3059_amd64.deb
 dpkg -i sublime-text_build-3059_amd64.deb
 touch "sublime.done"
 
 echo "Installing Vim"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install vim
 touch "vim.done"
 
 echo "Installing Emacs"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install emacs
 touch "emacs.done"
 
 echo "Installing ruby-install"
-cd "$tempbuild"
+cd "${tempbuild}"
 wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
 tar -xzf ruby-install-0.4.3.tar.gz
 cd ruby-install-0.4.3/
 make install
-cd "$tempbuild"
+cd "${tempbuild}"
 touch "ruby-install.done"
 
 echo "Installing ruby"
-cd "$tempbuild"
+cd "${tempbuild}"
 ruby-install ruby 2.1.2
 touch "ruby.done"
 
 echo "Installing chruby"
-cd "$tempbuild"
+cd "${tempbuild}"
 wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
 tar -xzf chruby-0.3.8.tar.gz
 cd chruby-0.3.8/
@@ -59,33 +59,33 @@ echo "" >> /etc/skel/.bashrc
 echo "source '/usr/local/share/chruby/chruby.sh'" >> /etc/skel/.bashrc
 echo "source '/usr/local/share/chruby/auto.sh'" >> /etc/skel/.bashrc
 echo "chruby ruby-2.1.2" >> /etc/skel/.bashrc
-cd "$tempbuild"
+cd "${tempbuild}"
 touch "chruby.done"
 
 echo "Installing node.js & npm"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install nodejs npm
 touch "nodejs.done"
 
 echo "Installing Java"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install default-jdk
 touch "java.done"
 
 echo "Installing Minecraft"
-cd "$tempbuild"
+cd "${tempbuild}"
 add-apt-repository -y ppa:minecraft-installer-peeps/minecraft-installer
 apt-get -y -q update
 apt-get -y -q install minecraft-installer
 touch "minecraft.done"
 
 echo "Installing Git"
-cd "$tempbuild"
+cd "${tempbuild}"
 apt-get -y -q install git
 touch "git.done"
 
 echo "Installing wallpaper"
-cd "$tempbuild"
+cd "${tempbuild}"
 wget https://s3-us-west-1.amazonaws.com/mojombo-codestarter/codestarter-tree.jpg
 cp codestarter-tree.jpg /usr/share/backgrounds
 touch "wallpaper.done"
@@ -95,7 +95,7 @@ touch "wallpaper.done"
 # 2. Sets trackpad scrolling to "natural" (same as default on OSX).
 # 3. Sets the desktop background to custom Codestarter wallpaper.
 echo "Installing dconf overrides"
-cd "$tempbuild"
+cd "${tempbuild}"
 echo -e "[com.canonical.Unity.Launcher]\n\
 \n\
 favorites=['application://nautilus.desktop', 'application://google-chrome.desktop', 'application://sublime_text.desktop', 'application://gnome-terminal.desktop', 'application://minecraft.desktop', 'application://ubuntu-software-center.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']
@@ -111,4 +111,4 @@ glib-compile-schemas /usr/share/glib-2.0/schemas/
 touch "dconf-overrides.done"
 
 # Cleanup
-rm -fr /tmp/tmp.*
+rm -rf "${tempbuild}"
